1d5f89cea28bd2ab257e363a1b18af6de491d0a5
PashockACDC <pashockacdc@mail.ru>
2016-08-06 00:00:22 +0300

    (fix) add initialisations in MainActivity class

--- src/main/java/com/example/firstapplication/simpleintegrals/MainActivity.java
+++ src/main/java/com/example/firstapplication/simpleintegrals/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.firstapplication.simpleintegrals;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
-    TextView tv_a;
-    TextView tv_b;
-    TextView tv_n;
+
+    EditText tv_a;
+    EditText tv_b;
+    EditText tv_n;
     TextView tv_resbyAntiderivative;
     TextView tv_resbyMiddleRectangles;
     TextView tv_resbyTrapeziums;
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     CheckBox chb_1_byMiddleRectangle;
     CheckBox chb_2_byTrapeziums;
     CheckBox chb_3_bySimpson;
+    RadioButton rbFunction1;
+    RadioButton rbFunction2;
     ProgressBar progressBar;
 
     @Override
@@ -30,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv_a = (TextView)findViewById(R.id.tv_a);
-        tv_b = (TextView)findViewById(R.id.tv_b);
-        tv_n = (TextView)findViewById(R.id.tv_n);
+        tv_a = (EditText)findViewById(R.id.tv_a);
+        tv_b = (EditText)findViewById(R.id.tv_b);
+        tv_n = (EditText)findViewById(R.id.tv_n);
         tv_resbyAntiderivative = (TextView)findViewById(R.id.tv_resbyAntiderivative);
         tv_resbyMiddleRectangles = (TextView)findViewById(R.id.tv_resbyMiddleRectangles);
         tv_resbyTrapeziums = (TextView)findViewById(R.id.tv_resbyTrapeziums);
@@ -45,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
         chb_1_byMiddleRectangle = (CheckBox)findViewById(R.id.chb_1_byMiddleRectangles);
         chb_2_byTrapeziums = (CheckBox)findViewById(R.id.chb_2_byTrapeziums);
         chb_3_bySimpson = (CheckBox)findViewById(R.id.chb_3_bySimpson);
+        rbFunction1 = (RadioButton)findViewById(R.id.rbFunction1);
+        rbFunction2 = (RadioButton)findViewById(R.id.rbFunction2);
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
 
     }
